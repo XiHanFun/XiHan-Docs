55a17a74170e312058b03f3129a19c90672036a3
ZhaiFanhua <zhaifanhua@foxmail.com>
2022-11-28 04:56:02 +0800

    优化：提交忽略文件；

--- zhaifanhuablog-docs/docs/.vuepress/.temp/internal/siteData.js
+++ zhaifanhuablog-docs/docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"摘繁华博客官方文档\",\"description\":\"摘繁华博客官方文档\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/images/logo.png\"}]],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
