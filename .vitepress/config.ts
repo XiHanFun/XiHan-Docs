55aa6dc9bc720398c7e9cee8280cb4ababb50bce
ZhaiFanhua <zhaifanhua@foxmail.com>
2023-06-14 20:16:17 +0800

    优化：首页添加变更日志；

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -25,6 +25,11 @@ export default defineConfig({
     nav: [
       { text: "项目简介", link: "/guide/", activeMatch: "/guide/" },
       {
+        text: "变更日志",
+        link: "/guide/ChangeLog.md",
+        activeMatch: "/guide/ChangeLog.md",
+      },
+      {
         text: "探索未知",
         items: [
           {
